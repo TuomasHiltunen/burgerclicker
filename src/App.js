6818b90f19cfd00f62cb8f561d7eb1ff322a8f32
Tuomas Hiltunen <hiltunen.tuomas@hotmail.com>
2020-04-17 10:29:58 +0300

    data storing to localstorage

--- src/App.js
+++ src/App.js
@@ -15,9 +15,14 @@ class Clicker extends Component {
 
   constructor (props){
     super (props);
+    let clicks = parseInt(localStorage.getItem("clicks"));
+    clicks = clicks ? clicks : 0;
+    let coupons = localStorage.getItem("coupons");
+    coupons = coupons ? JSON.parse(coupons) : [];
+
     this.state = {
-      clicks: 0,
-      coupons: [],
+      clicks: clicks,
+      coupons: coupons,
       claimableCoupons: 0,
       countUpdateValue: 0
     }
@@ -38,8 +43,8 @@ class Clicker extends Component {
       if (coupon.price <= clicks) {
         coupons++;
       }
-      if (updateValue < clicks && coupon.price > updateValue || 
-        coupon.price > clicks && coupon.price < updateValue ) {
+      if ((updateValue < clicks && coupon.price > updateValue) || 
+        (coupon.price > clicks && coupon.price < updateValue )) {
         updateValue = coupon.price;
       }
       this.setState ({
@@ -56,6 +61,7 @@ class Clicker extends Component {
     if (clicks > this.state.countUpdateValue) {
       this.updateCouponCount(clicks);
     }
+    localStorage.setItem("clicks",clicks);
   }
 
 claimCoupon(couponId) {
@@ -72,6 +78,8 @@ claimCoupon(couponId) {
     coupons: coupons
   });
   this.updateCouponCount(clicks);
+  localStorage.setItem("clicks", clicks);
+  localStorage.setItem("coupons", JSON.stringify(coupons));
 }
 
   render(){
